3d158c16b424f1214d5ec130e66a0ea9c361fc67
Volodymyr Alputov <alputov1998@gmail.com>
2024-01-09 17:21:56 +0200

    CHANGE: changed api server

--- src/api/axiosHelper.js
+++ src/api/axiosHelper.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:5001/';
+axios.defaults.baseURL = 'https://test-task-server-jzuc.onrender.com/';
 
 const axiosInstance = axios.create();
 
